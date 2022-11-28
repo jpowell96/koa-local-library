307b71971826836f1b02f7a0cb261bcd678336fa
Jonathan Powell <powell.jonj@gmail.com>
2022-11-28 15:12:53 -0500

    Refactor book props to Book domain class

--- src/book/domain/Book.ts
+++ src/book/domain/Book.ts
@@ -1,5 +1,18 @@
-export interface Book {
+export interface BookProps {
     title: String,
     authors: String[],
     id: Number
+}
+
+export class Book {
+    private readonly title : String;
+    private readonly authors: String[];
+    private readonly id: Number;
+
+    constructor(bookProps: BookProps) {
+        this.title = bookProps.title;
+        this.authors = bookProps.authors;
+        this.id = bookProps.id;
+    }
+
 }
