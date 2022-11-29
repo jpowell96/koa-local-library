d93912137e01bc17ab76226d6688dec3d0b9c14a
Jonathan Powell <powell.jonj@gmail.com>
2022-11-29 00:07:39 -0500

    Add some comments for tomorrow

--- server/src/book/repository/PostgresBookRepository.ts
+++ server/src/book/repository/PostgresBookRepository.ts
@@ -1,20 +1,26 @@
 import { Book } from '../domain/Book';
 import { GetBookByIdRequest, IBookRepository, ListBooksRequest } from './IBookRepository';
 import {Client, ClientBase} from "pg";
+
+// TODO: Make this a singleton where we only connect once. For now, pull in creds in the class.
+// reference point -  https://www.jonmellman.com/posts/singleton-promises
 export class PostgresBookRepository implements IBookRepository {
-    private readonly datasource : any;
+    private readonly postgresClient : ClientBase;
 
-    constructor(datasource: ClientBase) {
-        this.datasource = datasource;
+    constructor(postgresClient: ClientBase) {
+        this.postgresClient = postgresClient;
     }
 
 
-    listBooks(request: ListBooksRequest) : Promise<Book[]> {
-        return this.datasource.query('')
+   async listBooks(request: ListBooksRequest) : Promise<Book[]> {
+       await this.postgresClient.connect();
+       await this.postgresClient.query("SELECT * FROM ")
+
+       return Promise.reject();
     }
 
 
-    getBookById(request: GetBookByIdRequest) : Promise<Book> {
+    async getBookById(request: GetBookByIdRequest) : Promise<Book> {
         return Promise.reject();
     }
 
